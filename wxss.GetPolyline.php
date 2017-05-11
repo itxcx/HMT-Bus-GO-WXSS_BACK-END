a8a13bdb740c555be490f834b1177a1e165bff34
CRH380A-2722 <609657831@qq.com>
2017-05-11 10:54:03 +0800

    -

--- wxss.GetPolyline.php
+++ wxss.GetPolyline.php
@@ -2,9 +2,9 @@
 /**
  *  HMT Bus GO! (WXSS VER.)
  *
- *  @author CRH380A-2722 <609657831@qq.com>
- *  @copyright 2016-2017 CRH380A-2722
- *  @license MIT
+ *	@author CRH380A-2722 <609657831@qq.com>
+ *	@copyright 2016-2017 CRH380A-2722
+ *	@license MIT
  *	@note 获取线路路径
  */
 
